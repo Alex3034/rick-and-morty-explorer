aac1d174ecda26ced8ed7aca11c0c5645f7cf68b
Alejandro <alex1348wk@gmail.com>
2026-02-17 21:16:38 +0100

    Update render.js

--- src/render.js
+++ src/render.js
@@ -7,13 +7,14 @@ export function renderCharacters(characters) {
     characters.forEach(character => {
         // ----- Card -----
         const card = document.createElement("div");
-        card.className = "bg-gray-800 rounded-lg p-4 shadow-lg";
-
-        card.innerHTML = `
-            <img src="${character.image}" alt="${character.name}" class="rounded-md mb-4">
-            <h2 class="text-xl font-semibold text-green-400">${character.name}</h2>
-            <p>Status: ${character.status}</p>
-            <p>Species: ${character.species}</p>
+        card.className = `
+            bg-gray-800 
+            rounded-lg 
+            p-4 
+            shadow-lg 
+            hover:scale-105 
+            transition-transform 
+            duration-300
         `;
 
         // ----- Imagen -----
